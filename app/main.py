e163c635660bab3110d69ae432f1ae270225bcbe
Vickysahu12 <bikisahu161@gmail.com>
2026-04-29 13:16:13 +0530

    Add an admin route in main file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.middleware.cors import CORSMiddleware
-from app.db.database import engine,Base
+from app.db.database import engine,Base,get_db
 from app.routers import auth,Vocabss,Home,rc,article,va,notification
 from app.routers.mock import router as mock_router, attempts_router
 from app.routers.profile import router as profile_router
 from app.routers import admin
+
 
 
 app = FastAPI(
@@ -35,6 +38,15 @@ app.include_router(profile_router)
 app.include_router(admin.router)
 
 # Startup pe DB connection check and health check
+@app.get("/make-admin")
+async def make_admin(email: str, db: AsyncSession = Depends(get_db)):
+    await db.execute(
+        text("UPDATE users SET is_admin = true WHERE email = :email"),
+        {"email": email}
+    )
+    await db.commit()
+    return {"message": "Done!"}
+
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
